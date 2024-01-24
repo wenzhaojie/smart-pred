31b10a7de58c49eba1e6c30ea3d5dc9e8228f63e
wenzh <wenzhaojie@foxmail.com>
2024-01-24 16:47:39 +0800

    Update

--- smart_pred/model/gluonts_model.py
+++ smart_pred/model/gluonts_model.py
@@ -54,6 +54,7 @@ class GluonTS_model(Basic_model):
             trainer = GluonTrainer(epochs=100, num_batches_per_epoch=50)
             self.model = DeepStateEstimator(
                 freq=self.model_parameters["freq"],
+                cardinality=None,
                 prediction_length=self.model_parameters["pred_len"],
                 context_length=self.model_parameters["seq_len"],
                 use_feat_static_cat=False,
